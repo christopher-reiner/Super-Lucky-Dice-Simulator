1013e8ad5f2a7e6632a288254023b21c4d3a83dc
Christopher Reiner <creiner@mail.usf.edu>
2020-02-19 09:34:40 -0500

    Add files via upload

---
+++ Software System Design Project 1 Sandbox/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace Software_System_Design_Project_1_Sandbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.populateChartButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.numTestRollsLabel = new System.Windows.Forms.Label();
+            this.numTestRollsTextbox = new System.Windows.Forms.TextBox();
+            this.seedLabel = new System.Windows.Forms.Label();
+            this.seedTextBox = new System.Windows.Forms.TextBox();
+            this.chartResetButton = new System.Windows.Forms.Button();
+            this.finalResultsTextBox = new System.Windows.Forms.RichTextBox();
+            this.testResultsLabel = new System.Windows.Forms.Label();
+            this.printResultsButton = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.die1Label = new System.Windows.Forms.Label();
+            this.die2Label = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // populateChartButton
+            // 
+            this.populateChartButton.Location = new System.Drawing.Point(25, 606);
+            this.populateChartButton.Name = "populateChartButton";
+            this.populateChartButton.Size = new System.Drawing.Size(233, 98);
+            this.populateChartButton.TabIndex = 1;
+            this.populateChartButton.Text = "Populate Charts";
+            this.populateChartButton.UseVisualStyleBackColor = true;
+            this.populateChartButton.Click += new System.EventHandler(this.populateChartButton_click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            // 
+            // numTestRollsLabel
+            // 
+            this.numTestRollsLabel.AutoSize = true;
+            this.numTestRollsLabel.Location = new System.Drawing.Point(12, 490);
+            this.numTestRollsLabel.Name = "numTestRollsLabel";
+            this.numTestRollsLabel.Size = new System.Drawing.Size(114, 25);
+            this.numTestRollsLabel.TabIndex = 7;
+            this.numTestRollsLabel.Text = "# Test Rolls";
+            // 
+            // numTestRollsTextbox
+            // 
+            this.numTestRollsTextbox.Location = new System.Drawing.Point(152, 490);
+            this.numTestRollsTextbox.Name = "numTestRollsTextbox";
+            this.numTestRollsTextbox.Size = new System.Drawing.Size(106, 29);
+            this.numTestRollsTextbox.TabIndex = 6;
+            this.numTestRollsTextbox.Text = "6000";
+            // 
+            // seedLabel
+            // 
+            this.seedLabel.AutoSize = true;
+            this.seedLabel.Location = new System.Drawing.Point(20, 543);
+            this.seedLabel.Name = "seedLabel";
+            this.seedLabel.Size = new System.Drawing.Size(115, 25);
+            this.seedLabel.TabIndex = 10;
+            this.seedLabel.Text = "Seed Value";
+            // 
+            // seedTextBox
+            // 
+            this.seedTextBox.Location = new System.Drawing.Point(152, 539);
+            this.seedTextBox.Name = "seedTextBox";
+            this.seedTextBox.Size = new System.Drawing.Size(106, 29);
+            this.seedTextBox.TabIndex = 9;
+            this.seedTextBox.Text = "6000";
+            // 
+            // chartResetButton
+            // 
+            this.chartResetButton.Location = new System.Drawing.Point(25, 726);
+            this.chartResetButton.Name = "chartResetButton";
+            this.chartResetButton.Size = new System.Drawing.Size(233, 93);
+            this.chartResetButton.TabIndex = 11;
+            this.chartResetButton.Text = "Reset Charts";
+            this.chartResetButton.UseVisualStyleBackColor = true;
+            this.chartResetButton.Click += new System.EventHandler(this.chartResetButton_Click);
+            // 
+            // finalResultsTextBox
+            // 
+            this.finalResultsTextBox.Location = new System.Drawing.Point(281, 490);
+            this.finalResultsTextBox.Name = "finalResultsTextBox";
+            this.finalResultsTextBox.Size = new System.Drawing.Size(488, 454);
+            this.finalResultsTextBox.TabIndex = 12;
+            this.finalResultsTextBox.Text = "";
+            // 
+            // testResultsLabel
+            // 
+            this.testResultsLabel.AutoSize = true;
+            this.testResultsLabel.Location = new System.Drawing.Point(460, 452);
+            this.testResultsLabel.Name = "testResultsLabel";
+            this.testResultsLabel.Size = new System.Drawing.Size(120, 25);
+            this.testResultsLabel.TabIndex = 13;
+            this.testResultsLabel.Text = "Test Results";
+            // 
+            // printResultsButton
+            // 
+            this.printResultsButton.Location = new System.Drawing.Point(25, 848);
+            this.printResultsButton.Name = "printResultsButton";
+            this.printResultsButton.Size = new System.Drawing.Size(233, 96);
+            this.printResultsButton.TabIndex = 14;
+            this.printResultsButton.Text = "Print Results";
+            this.printResultsButton.UseVisualStyleBackColor = true;
+            this.printResultsButton.Click += new System.EventHandler(this.printResults);
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.chart1.BorderlineColor = System.Drawing.Color.DimGray;
+            this.chart1.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(25, 57);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Pastel;
+            series1.ChartArea = "ChartArea1";
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "Die #1 Rolls";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(744, 374);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            this.chart2.BackColor = System.Drawing.Color.DarkSeaGreen;
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(827, 57);
+            this.chart2.Name = "chart2";
+            this.chart2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Pastel;
+            series2.ChartArea = "ChartArea1";
+            series2.IsVisibleInLegend = false;
+            series2.Legend = "Legend1";
+            series2.Name = "Die #2 Rolls";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(751, 374);
+            this.chart2.TabIndex = 29;
+            this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            this.chart3.BackColor = System.Drawing.Color.DarkSeaGreen;
+            chartArea3.AxisX.IntervalOffset = 1D;
+            chartArea3.AxisX2.Maximum = 12D;
+            chartArea3.AxisX2.Minimum = 2D;
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart3.Legends.Add(legend3);
+            this.chart3.Location = new System.Drawing.Point(827, 490);
+            this.chart3.Name = "chart3";
+            this.chart3.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Pastel;
+            series3.ChartArea = "ChartArea1";
+            series3.IsVisibleInLegend = false;
+            series3.Legend = "Legend1";
+            series3.Name = "Combined Die Rolls";
+            this.chart3.Series.Add(series3);
+            this.chart3.Size = new System.Drawing.Size(751, 454);
+            this.chart3.TabIndex = 30;
+            this.chart3.Text = "chart3";
+            // 
+            // die1Label
+            // 
+            this.die1Label.AutoSize = true;
+            this.die1Label.Location = new System.Drawing.Point(350, 29);
+            this.die1Label.Name = "die1Label";
+            this.die1Label.Size = new System.Drawing.Size(68, 25);
+            this.die1Label.TabIndex = 31;
+            this.die1Label.Text = "Die #1";
+            // 
+            // die2Label
+            // 
+            this.die2Label.AutoSize = true;
+            this.die2Label.Location = new System.Drawing.Point(1170, 29);
+            this.die2Label.Name = "die2Label";
+            this.die2Label.Size = new System.Drawing.Size(68, 25);
+            this.die2Label.TabIndex = 32;
+            this.die2Label.Text = "Die #2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1110, 452);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 25);
+            this.label1.TabIndex = 33;
+            this.label1.Text = "Combined Die Scores";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1604, 956);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.die2Label);
+            this.Controls.Add(this.die1Label);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.printResultsButton);
+            this.Controls.Add(this.testResultsLabel);
+            this.Controls.Add(this.finalResultsTextBox);
+            this.Controls.Add(this.chartResetButton);
+            this.Controls.Add(this.seedLabel);
+            this.Controls.Add(this.seedTextBox);
+            this.Controls.Add(this.numTestRollsLabel);
+            this.Controls.Add(this.numTestRollsTextbox);
+            this.Controls.Add(this.populateChartButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1628, 1020);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1628, 1020);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " Super Lucky Dice Simulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button populateChartButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label numTestRollsLabel;
+        private System.Windows.Forms.TextBox numTestRollsTextbox;
+        private System.Windows.Forms.Label seedLabel;
+        private System.Windows.Forms.TextBox seedTextBox;
+        private System.Windows.Forms.Button chartResetButton;
+        private System.Windows.Forms.RichTextBox finalResultsTextBox;
+        private System.Windows.Forms.Label testResultsLabel;
+        private System.Windows.Forms.Button printResultsButton;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.Label die1Label;
+        private System.Windows.Forms.Label die2Label;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Software System Design Project 1 Sandbox/Form1.cs
@@ -0,0 +1,228 @@
+﻿//===================================================================================================================================
+//Author: Christopher Reiner
+//The University of South Florida
+//COP4365 Software System Design
+//Project #1 - Due 02-17-20
+//Objective:
+//Write a program to simulate a uniform (meaning every number is equally likely to appear as the next) integer random number.  
+//Your program will be a console application which will simulate a 6-faced die. You will “throw” your simulated die N times. 
+//The number N should be obtained by prompting the user on “How many rolls?” Also, since you will be using the Random class 
+//provide a prompt requesting that the user provide a seed number.The prompt can be as simple as "Please enter a seed:" and 
+//a text box. Then use the seed provided by the user to seed the random number generator. This will also help you test your program.
+//We will use 6000 times as an example. Show the frequency distribution of your numbers when 6,000 tosses are made and the 
+//random number is instantiated to generate numbers between 1 and 6 (inclusive).  In other words, show how many times the 
+//numbers 1,2,3,….and 6 have appeared.The output displayed on the screen must use the chart object. 
+//Then have another chart to display the histogram obtained when you roll 2 dice and add up the numbers that come up (2 – 12). 
+//As in the first part, you will roll the dice N times.
+//===================================================================================================================================
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Software_System_Design_Project_1_Sandbox
+{
+    public partial class Form1 : Form
+    {
+        //declares global variables
+        List<int> rollCounts = new List<int>();
+        List<int> rollCounts2 = new List<int>();
+        List<int> rollCounts3 = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //message box to prompt the user with instructions
+            Interaction.MsgBox("Welcome to the Super Lucky Dice Simulator. This program provides your casino with randomly thrown " +
+                                 "die rolls for your customers to game with. The simulator features a \"seed\" feature, which may be " +
+                                 "set by you the owner when preparing the simulator to generate die tosses. The program begins by " +
+                                 "prompting you to enter a seed, which is then followed by request for you to enter the desired # of " +
+                                 "rolls to use in the simulator. Both of these values can be updated at any time during the simulation. " +
+                                 "The results for each die throw are shown on the corresponding table, and the distribution of the total " +
+                                 "scores can be observed on a third. There are fields for you to enter the seed and the number of rolls. The " +
+                                 "\"Populate Charts\" button will start the simulation based on the current parameters. The \"Reset Charts\" " +
+                                 "button clears and resets all charts and data. The \"Print Results\" button creates a text file of the data " +
+                                 "found in the Test Results field that can be used to verify your system with your potential investors. The " +
+                                 "program is ended by clicking the traditional \"close\" box on the top right corner.");
+
+            //input box prompt to set seed value
+            string seedInput;
+            seedInput = Interaction.InputBox("Please enter a seed value...\n\n" +
+                        "Note: You may update this value at any time once the program has launched.", "Initializing Seed Value", "0", -1, -1);
+
+            //local variables and while loop to check for correct seed input
+            bool seedValid = false;
+            int Number;
+            while (seedValid == false)
+            {
+                if (int.TryParse(seedInput, out Number))
+                {
+                    seedValid = true;
+                }
+                else
+                {
+                    seedInput = Interaction.InputBox("Please enter a seed value...\n\n" +
+                        "Note: You may update this value at any time once the program has launched.", "INVALID INPUT - PLEASE ENTER A WHOLE NUMBER", "0", -1, -1);
+                }
+            }
+
+            //populates seed text box once valid input received
+            seedTextBox.Text = seedInput;
+
+            //input box prompt to set number of rolls value
+            string numRollsInput;
+            numRollsInput = Interaction.InputBox("Please enter the number of rolls to use for the test...\n\n" +
+                        "Note: You may update this value at any time once the program has launched.", "Initializing Number Of Rolls", "6000", -1, -1);
+
+            //local variables and while loop to check for correct seed input
+            bool numRollsValid = false;
+            while (numRollsValid == false)
+            {
+                if (int.TryParse(numRollsInput, out Number) && (Convert.ToInt32(numRollsInput) > 0))
+                {
+                    numRollsValid = true;
+                }
+                else
+                {
+                    numRollsInput = Interaction.InputBox("\nPlease enter the number of rolls to use for the test...\n\n" +
+                        "Note: You may update this value at any time once the program has launched.", "INVALID INPUT - PLEASE ENTER A WHOLE NUMBER > 0", "6000", -1, -1);
+                }
+            }
+            //populates numRolls text box once valid input received
+            numTestRollsTextbox.Text = numRollsInput;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //calls fillChart function so tables are populated with the current parameters
+            fillChart();
+        }
+
+        //function for reseting and populating table parameter based on current input
+        private void fillChart()
+        {
+            //detects if chart 1 and chart 2 have been previously created and if so resets them
+            if (rollCounts.Count() > 0)
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    rollCounts.RemoveAt(0);
+                    chart1.Series["Die #1 Rolls"].Points.RemoveAt(0);
+                    rollCounts2.RemoveAt(0);
+                    chart2.Series["Die #2 Rolls"].Points.RemoveAt(0);
+                }
+            }
+
+            //detects if chart 3 has been previously created and if so resets it
+            if (rollCounts3.Count() > 0)
+            {
+                for (int i = 0; i < 11; i++)
+                {
+                    rollCounts3.RemoveAt(0);
+                    chart3.Series["Combined Die Rolls"].Points.RemoveAt(0);
+                }
+            }
+
+            //sets the Y axis depending the anticipated number of rolls
+            double yNumber = (Convert.ToInt32(numTestRollsTextbox.Text) / 6) * 1.2;
+            if (yNumber < 20) yNumber = 20;
+
+            //populates the # of columns and sets the labels for the individual die tables
+            for (int i = 0; i < 6; i++)
+            {
+                chart1.Series["Die #1 Rolls"].Points.AddXY(Convert.ToString(i + 1) + "pt", "0");
+                chart2.Series["Die #2 Rolls"].Points.AddXY(Convert.ToString(i + 1) + "pt", "0");
+                rollCounts.Add(0);
+                rollCounts2.Add(0);
+            }
+
+            //populates the # of columns and sets the labels for the combined die score tables
+            for (int i = 0; i < 11; i++)
+            {
+                chart3.Series["Combined Die Rolls"].Points.AddXY(Convert.ToString(i + 2) + "pt", "0");
+                rollCounts3.Add(0);
+            }
+
+            //sets additional parameters for the tables
+            chart1.ChartAreas[0].AxisY.Maximum = yNumber;
+            chart2.ChartAreas[0].AxisY.Maximum = yNumber;
+            chart3.ChartAreas[0].AxisY.Maximum = 1.5 * yNumber;
+            chart3.ChartAreas[0].AxisX.Interval = 1;
+        }
+
+        //function to perform random rolls and populate tables accordingly
+        private void populateChartButton_click(object sender, EventArgs e)
+        {
+            //clears charts for new data
+            fillChart();
+
+            //calls an instance of a random class
+            Random randomValue = new Random(Convert.ToInt32(seedTextBox.Text));
+
+            //for loop for performing X number of random die throws and populating tables with that data
+            for (int i = 0; i < Convert.ToInt32(numTestRollsTextbox.Text); i++)
+            {
+                //random call for die #1 recorded in List and applied to table
+                int roll = randomValue.Next(1, 7);
+                rollCounts[roll - 1]++;
+                chart1.Series["Die #1 Rolls"].Points[roll - 1].SetValueY(rollCounts[roll - 1]);
+
+                //random call for die #2 recorded in List and applied to table
+                int roll2 = randomValue.Next(1, 7);
+                rollCounts2[roll2 - 1]++;
+                chart2.Series["Die #2 Rolls"].Points[roll2 - 1].SetValueY(rollCounts2[roll2 - 1]);
+
+                //combines die scores and records that result to third List
+                rollCounts3[roll + roll2 - 2]++;
+                chart3.Series["Combined Die Rolls"].Points[roll + roll2 - 2].SetValueY(rollCounts3[roll + roll2 - 2]);
+
+                //redraws charts based on updated info
+                chart1.Refresh();
+                chart2.Refresh();
+                chart3.Refresh();
+            }
+
+            //sets parameters for final results textbox
+            finalResultsTextBox.SelectionFont = new Font("Calibri", 12, FontStyle.Bold);
+            finalResultsTextBox.SelectionColor = System.Drawing.Color.Blue;
+
+            //prints results to test results text box
+            finalResultsTextBox.Text += "Seed Value: " + seedTextBox.Text + "\nNumber of Rolls: " + numTestRollsTextbox.Text + "\n\n";
+            finalResultsTextBox.Text += "Die #1: \n";
+            for (int i = 0; i < 6; i++)
+            {
+                finalResultsTextBox.Text += Convert.ToString(i + 1) + " was rolled " + Convert.ToString(rollCounts[i]) + " times.\n";
+            }
+            finalResultsTextBox.Text += "\n";
+            finalResultsTextBox.Text += "Die #2: \n";
+            for (int i = 0; i < 6; i++)
+            {
+                finalResultsTextBox.Text += Convert.ToString(i + 1) + " was rolled " + Convert.ToString(rollCounts2[i]) + " times.\n";
+            }
+            finalResultsTextBox.Text += "\n";
+            for (int i = 0; i < 11; i++)
+            {
+                finalResultsTextBox.Text += "A total score of " + Convert.ToString(i + 2) + " was rolled " + Convert.ToString(rollCounts3[i]) + " times.\n";
+            }
+        }
+
+        //function that creates a txt. file for record keeping
+        private void printResults(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("Single Die Results.txt");
+            sw.WriteLine(finalResultsTextBox.Text);
+            sw.Close();
+        }
+
+        //function that resets both the charts and the final results text box
+        private void chartResetButton_Click(object sender, EventArgs e)
+        {
+            fillChart();
+            finalResultsTextBox.Clear();
+        }
+    }
+}
